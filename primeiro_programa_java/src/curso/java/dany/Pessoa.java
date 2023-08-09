d3e1c700190cbb76b5db944c42d293e19ed8e878
Daniele_Gutinik <Daniele_Gutinik@DESKTOP-26JSHEK>
2023-08-09 14:28:36 -0300

    Alteração e criação da Classe pessoa

---
+++ primeiro_programa_java/src/curso/java/dany/Pessoa.java
@@ -0,0 +1,26 @@
+package curso.java.dany;
+
+public class Pessoa {
+	
+	private String nome;
+	
+	private String Sobrenome;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSobrenome() {
+		return Sobrenome;
+	}
+
+	public void setSobrenome(String sobrenome) {
+		Sobrenome = sobrenome;
+	}
+	
+	
+}
